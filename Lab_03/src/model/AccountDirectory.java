8d394a369472f9a0f9a13721cf19188415334b4b
mitra <mitra@Aditya-Mitra.local>
2024-09-29 22:46:45 -0400

    final lab 3 commit

---
+++ Lab_03/src/model/AccountDirectory.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author mitra
+ */
+public class AccountDirectory {
+    
+    private ArrayList<Account> accounts;
+    public AccountDirectory()
+    {
+        this.accounts = new ArrayList<Account>();
+        
+    }
+    public ArrayList<Account> getAccount()
+    {
+        return accounts;
+        
+    }
+    public void setAccounts(ArrayList<Account> accounts)
+    {
+        this.accounts = accounts;
+        
+    }
+    public Account addAccount()
+    {
+        Account a = new Account();
+        accounts.add(a);
+        return a;
+    }
+    public void deleteAccount(Account account)
+    {
+        accounts.remove(account);
+    }
+    public Account SearchAccount(String accountNumber)
+    {
+        for(Account a: accounts)
+        {
+            if(a.getAccountNumber().contains(accountNumber)){
+                return a;
+            }
+        }
+        return null;
+    }  
+    
+}
