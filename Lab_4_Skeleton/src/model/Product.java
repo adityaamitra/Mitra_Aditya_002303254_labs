4dd8781d9952f1a90fc30fd9224dc0aeaea801f8
mitra <mitra@Aditya-Mitra.local>
2024-10-13 21:05:21 -0400

    Product Image functionality Implemented

---
+++ Lab_4_Skeleton/src/model/Product.java
@@ -0,0 +1,100 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model;
+
+import java.util.ArrayList;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Rushabh
+ */
+public class Product {
+    
+    private String name;
+    private int price;
+    private ImageIcon logoImage;
+
+    public ImageIcon getLogoImage() {
+        return logoImage;
+    }
+
+    public void setLogoImage(ImageIcon logoImage) {
+        this.logoImage = logoImage;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    private int id;
+
+
+    public ArrayList<Feature> getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(ArrayList<Feature> features) {
+        this.features = features;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public static void setCount(int count) {
+        Product.count = count;
+    }
+    
+    private ArrayList<Feature> features;
+
+    private static int count = 0;
+    
+    public Product() {
+        count++;
+        id = count;
+        
+        features = new ArrayList<Feature>();
+        
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    
+    
+    
+    
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public Feature addNewFeature() {
+        
+        return null;
+        
+    }
+    
+}
