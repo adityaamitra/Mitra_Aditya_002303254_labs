33d163a6964709721c2851af57b9f16b415a4fd2
mitra <mitra@Aditya-Mitra.local>
2024-11-23 02:10:15 -0500

    Overall User Function

--- SocialMedia_Skeleton/src/analytics/AnalysisHelper.java
+++ SocialMedia_Skeleton/src/analytics/AnalysisHelper.java
@@ -115,5 +115,41 @@ public class AnalysisHelper {
             System.out.println(users.get(i) + ", - Comment count: " + commentNumbers.get(users.get(i).getId()));
         }
     }
-    
+    public void getPassiveAndActiveoverallUsers () {
+        
+        DataStore data = DataStore.getInstance() ;
+        HashMap<Integer, Integer> overallNumbers = new HashMap<Integer, Integer> ();
+        for (Comment c : data.getComments().values()) {
+            int userId = c.getUserId();
+            if (overallNumbers.containsKey(userId)) {
+                overallNumbers.put(userId, overallNumbers. get (userId) + 1 + c.getLikes());
+            } else {
+                overallNumbers.put(userId, 1 + c.getLikes());
+            }
+        }
+        for (Post p: data.getPosts (). values ()) {
+        int userId = p.getUserId ();
+        if (overallNumbers.containsKey(userId)) {
+            overallNumbers.put(userId, overallNumbers.get(userId) + 1);
+        }
+        else {
+            overallNumbers. put (userId, 1);
+        }
+        }
+        
+        
+        ArrayList<User> users = new ArrayList(data.getUsers().values());
+        Collections.sort(users, new UserMapComparator(overallNumbers)) ;
+        
+        System.out.println("The Following Users Have Overall Been Passive: ");
+        for (int i = 0; i < 5; i++) {
+            System.out.println(users.get(i) + ", - Overall count: " + overallNumbers.get(users.get(i).getId()));
+        }
+        
+        
+        System.out.println("The Following Users Have Overall Been Active: ");
+        for (int i = users.size()-1; i > users.size()-6; i--) {
+           System.out.println(users.get(i) + ", - Overall count: " + overallNumbers.get(users.get(i).getId()));
+        }               
+    }
 }
